dcb016777f3e0a5c136b7fb2e455cddda25c37aa
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-11-28 23:48:11 +0900

    refactor: schemas

--- api/app/blog/main.py
+++ api/app/blog/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from .schemas import Blog
 
 app = FastAPI()
-
-class Blog(BaseModel):
-    title: str
-    body: str
 
 @app.post("/")
 def create(blog: Blog):
