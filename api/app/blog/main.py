5c21909cbc4390429052b98483d3050c3797791a
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-12-02 12:05:29 +0900

    add: POST method

--- api/app/blog/main.py
+++ api/app/blog/main.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from .schemas import Blog
+from .models import Base
+from . import models
+from .database import engine, sessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+Base.metadata.create_all(engine)
 
-@app.post("/")
-def create(blog: Blog):
-    return {'data': blog}
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/blog")
+def create(blog: Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, body=blog.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
