6c9ca76c856e9fb43bd669637adf689d1fe483b2
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-12-02 13:08:10 +0900

    add: status code

--- api/app/blog/main.py
+++ api/app/blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response
 from .schemas import Blog
 from .models import Base
 from . import models
@@ -24,13 +24,16 @@ def all_fetch(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get('/blogs/{id}')
-def show(id: int, db: Session = Depends(get_db)):
+@app.get('/blogs/{id}', status_code=status.HTTP_200_OK)
+def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'detail': f'Blog with the id {id} is not available'}
     return blog
 
 
-@app.post('/blogs')
+@app.post('/blogs', status_code=status.HTTP_201_CREATED)
 def create(blog: Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
