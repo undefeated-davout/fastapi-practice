4c5d5eeb804dc61c3a117d13a580c7ec8741d04c
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-12-04 14:57:53 +0900

    add: relation

--- api/app/blog/models.py
+++ api/app/blog/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 from .database import Base
 
 
@@ -9,6 +11,7 @@ class User(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
+    blogs = relationship('Blog', back_populates='creator')
 
 
 class Blog(Base):
@@ -17,3 +20,5 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    creator = relationship('User', back_populates='blogs')
