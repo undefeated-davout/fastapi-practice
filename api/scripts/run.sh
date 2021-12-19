bdc8ab13188b3395ddcd5c7af1d5e0e883de50e3
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-12-19 17:57:55 +0900

    add: 指定回数繰り返し

--- api/scripts/run.sh
+++ api/scripts/run.sh
@@ -2,9 +2,11 @@
 
 echo "Waiting for MySQL to start..."
 
-until mysqladmin ping -h "$MYSQL_HOST" -P "$MYSQL_TCP_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent; do
-echo "Waiting for MySQL retry ..."
-  sleep 1
+for i in `seq 0 30`
+do
+  mysqladmin ping -h "$MYSQL_HOST" -P "$MYSQL_TCP_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent
+  echo "Waiting for MySQL retry ..."
+  sleep 2
 done
 
 echo "MySQL is now ready"
