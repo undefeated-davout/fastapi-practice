3c8f6453c8bbbb0af7e39b8a613db685a2a03bdc
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-12-19 18:25:01 +0900

    add: DB接続チェック

--- db/scripts/run.sh
+++ db/scripts/run.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+RETRY_COUNT=30
+count=0
+
 echo "Waiting for MySQL to start..."
 
-for i in `seq 0 30`
+while true
 do
-  mysqladmin ping -h "$MYSQL_HOST" -P "$MYSQL_TCP_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent
-  echo "Waiting for MySQL retry ..."
-  sleep 2
+  mysqladmin ping -h "$MYSQL_HOST" -P "$MYSQL_TCP_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent && break
+  sleep 1
+  echo $count
+  count=`expr $count + 1`
+  if [ $count -eq $RETRY_COUNT ]; then
+    exit 1
+  fi
 done
 
 echo "MySQL is now ready"
