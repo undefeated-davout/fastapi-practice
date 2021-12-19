998938e4abc6d45874d68cd66ebeb50b7471b75e
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-12-19 22:43:08 +0900

    add: Webテスト

--- api/app/routes/user.py
+++ api/app/routes/user.py
@@ -2,10 +2,23 @@ from app.functions.user import create_user, show_user
 from app.http.request import UserReq
 from app.http.response import UserRes
 from app.utils.database import get_db
+from app.utils.oauth2 import get_current_user
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.get(
+    "/me",
+    status_code=status.HTTP_200_OK,
+    response_model=UserRes,
+)
+def read_users_me(
+    db: Session = Depends(get_db),
+    current_user: UserReq = Depends(get_current_user),
+):
+    return current_user
 
 
 @router.get(
